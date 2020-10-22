8ee46e6d11531984b4f24b5f1e7dbde8f13dcff1
eremin.v <v.eremin@apibank.online>
2020-10-22 22:55:40 +0300

    asd

--- src/main/java/org/example/controller/ClientController.java
+++ src/main/java/org/example/controller/ClientController.java
@@ -48,7 +48,7 @@ public class ClientController {
     }
 
     @PutMapping
-    public Client update(@RequestBody IdDto idDto) {
+    public Client update(@RequestBody UUID idDto) {
         //clientService.save()
         return null;
     }
